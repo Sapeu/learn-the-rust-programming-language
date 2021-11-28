12977bd1021cf4b42894ddfbcf31e62cde09abf5
Sapeu <sapeu.lee@foxmail.com>
2021-11-28 14:20:55 +0800

    5.3. 方法语法

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -6,15 +6,52 @@ struct Rectangle {
 
 fn main() {
     let scale = 2;
-    let rect1 = Rectangle { width: dbg!(123 * scale), height: 321 };
-        println!("rect1 is {:?}", rect1);
+    let rect1 = Rectangle {
+        width: dbg!(123 * scale),
+        height: 321,
+    };
+    println!("rect1 is {:?}", rect1);
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
     dbg!(&rect1);
+    if rect1.width_valid() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+
+    let rect2 = Rectangle {
+        width: 11,
+        height: 33,
+    };
+    let rect3 = Rectangle {
+        width: 666,
+        height: 555,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    dbg!(Rectangle::square(3));
 }
 
-fn area(r: &Rectangle) -> u32 {
-    r.width * r.height
+impl Rectangle {
+    // &self 为 self: &Self的缩写
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width_valid(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
