cf9a8a4018af8af402aa74c4223379ff9db71138
Sapeu <sapeu.lee@foxmail.com>
2021-11-28 15:53:14 +0800

    6.2. match 控制流运算符

---
+++ matches/src/main.rs
@@ -0,0 +1,53 @@
+#[derive(Debug)] // 这样可以立刻看到州的名称
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    println!("Hello, world!");
+    dbg!(value_in_cents(Coin::Quarter(UsState::Alaska)));
+    dbg!(value_in_cents(Coin::Penny));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    dbg!(six);
+    let none = plus_one(None);
+    dbg!(none);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
