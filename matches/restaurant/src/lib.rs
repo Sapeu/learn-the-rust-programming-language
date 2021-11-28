e079dce00297d57433cc3c2f9b7dc17605f96374
Sapeu <sapeu.lee@foxmail.com>
2021-11-28 17:00:19 +0800

    7.3. 路径用于引用模块树中的项

--- matches/restaurant/src/lib.rs
+++ matches/restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist(){}
+    pub mod hosting {
+        pub fn add_to_waitlist(){}
         fn seat_at_table(){}
     }
 
@@ -10,6 +10,52 @@ mod front_of_house {
         fn take_payment(){}
     }
 }
+
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast{
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order(){
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+}
+
+pub fn eat_at_resaurant() {
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+     // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    crate::front_of_house::hosting::add_to_waitlist();
+    front_of_house::hosting::add_to_waitlist();
+}
+
+fn serve_order(){}
 
 #[cfg(test)]
 mod tests {
