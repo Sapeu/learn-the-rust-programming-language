354b26d59d2b8fddda70a3d4e86c427498793843
Sapeu <sapeu.lee@foxmail.com>
2021-11-28 13:51:42 +0800

    5.2. 一个使用结构体的示例程序

---
+++ rectangles/src/main.rs
@@ -0,0 +1,20 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let scale = 2;
+    let rect1 = Rectangle { width: dbg!(123 * scale), height: 321 };
+        println!("rect1 is {:?}", rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+    dbg!(&rect1);
+}
+
+fn area(r: &Rectangle) -> u32 {
+    r.width * r.height
+}
