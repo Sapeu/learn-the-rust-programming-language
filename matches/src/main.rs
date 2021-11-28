54b754785bcd6b3527f2acebae3ebb713e9a9215
Sapeu <sapeu.lee@foxmail.com>
2021-11-28 16:07:30 +0800

    6.3. if let 简洁控制流

--- matches/src/main.rs
+++ matches/src/main.rs
@@ -23,13 +23,39 @@ fn main() {
     let none = plus_one(None);
     dbg!(none);
 
-    let some_u8_value = 0u8;
+    let some_u8_value = 3;
     match some_u8_value {
         1 => println!("one"),
         3 => println!("three"),
         5 => println!("five"),
         7 => println!("seven"),
         _ => (),
+    }
+
+    let some_u8_value = Some(3);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+
+    let coin = Coin::Quarter(UsState::Alaska);
+    let mut count = 0;
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+
+    let coin = Coin::Quarter(UsState::Alabama);
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
     }
 }
 
