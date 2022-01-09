085affc6166a3f63c27d3a969d3c085db9ef9286
Sapeu <sapeu.lee@foxmail.com>
2022-01-09 19:48:58 +0800

    10.2. trait：定义共享的行为

--- largest/src/main.rs
+++ largest/src/main.rs
@@ -21,24 +21,25 @@
 // 
 //     largest
 // }
-// 
-// fn largest<T>(list: &[T]) -> T {
-//     let mut largest = list[0];
-// 
-//     for &item in list.iter() {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
-// 
-//     largest
-// }
 
-#[derive(Debug)]
-struct Point<T, U> {
-    x: T,
-    y: U,
+fn largest<T>(list: &[T]) -> T
+where T: PartialOrd + Copy{
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
 }
+
+// #[derive(Debug)]
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
 
 // impl<T> Point<T> {
 //     fn x(&self) -> &T {
@@ -46,14 +47,14 @@ struct Point<T, U> {
 //     }
 // }
 
-impl<T, U> Point<T, U> {
-    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
-        Point {
-            x: self.x,
-            y: other.y,
-        }
-    }
-}
+// impl<T, U> Point<T, U> {
+//     fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+//         Point {
+//             x: self.x,
+//             y: other.y,
+//         }
+//     }
+// }
 
 // impl Point<f32> {
 //     fn distance_from_origin(&self) -> f32 {
@@ -62,21 +63,21 @@ impl<T, U> Point<T, U> {
 // }
 
 fn main() {
-    // let number_list = vec![34, 50, 25, 100, 65];
+    let number_list = vec![34, 50, 25, 100, 65];
 
-    // let result = largest(&number_list);
-    // println!("The largest number is {}", result);
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
-    // let char_list = vec!['y', 'm', 'a', 'q'];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    // let result = largest(&char_list);
-    // println!("The largest char is {}", result);
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 
-    let p1 = Point { x: 5, y: 10.6 };
-    println!("{:?}", p1);
-    let p2 = Point { x: "Hello", y: "Rust" };
-    println!("{:?}", p2);
-    let p3 = p1.mixup(p2);
-    println!("{:?}", p3);
-    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    // let p1 = Point { x: 5, y: 10.6 };
+    // println!("{:?}", p1);
+    // let p2 = Point { x: "Hello", y: "Rust" };
+    // println!("{:?}", p2);
+    // let p3 = p1.mixup(p2);
+    // println!("{:?}", p3);
+    // println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
---
+++ traits/src/lib.rs
@@ -0,0 +1,76 @@
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+    fn summarize(&self) -> String {
+        format!("Read more from {}...", self.summarize_author())
+    }
+
+    // fn summarize(&self) -> String {
+    //     String::from("Read more...")
+    // }
+}
+
+// pub fn notify<T: Summary>(item: T) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+// pub fn notify<T>(item: T)
+//     where T: Summary {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+pub fn notify(item: impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
